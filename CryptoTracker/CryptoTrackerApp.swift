fbbe9b125188b3852863521c4b53b151ee4f2122
wwq345 <wwq.bst@sap.com>
2022-03-16 17:24:13 +0800

    Initial Commit

---
+++ CryptoTracker/CryptoTrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CryptoTrackerApp.swift
+//  CryptoTracker
+//
+//  Created by i564206 on 2022/3/16.
+//
+
+import SwiftUI
+
+@main
+struct CryptoTrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
